35223d1d31e8577132a4b984cda48ca5d5f4a6a3
rturrado <68099809+rturrado@users.noreply.github.com>
2023-11-21 11:20:31 +0100

    Windows gtest doesn't like using raw literals with escaped quotes within EXPECT_EQ.

--- test/test_format_utils.cpp
+++ test/test_format_utils.cpp
@@ -15,14 +15,19 @@ TEST(indent_0_drop_1, empty_raw_literal) { EXPECT_EQ(R"()"_indent_0_drop_1, "");
 TEST(indent_0_drop_1, one_line_raw_literal) { EXPECT_EQ(R"(abc)"_indent_0_drop_1, ""); }
 TEST(indent_0_drop_1, one_line_raw_literal_with_quotes) { EXPECT_EQ(R"(abc"123")"_indent_0_drop_1, ""); }
 TEST(indent_0_drop_1, one_line_raw_literal_with_quotes_and_escaped_quotes) {
-    EXPECT_EQ(R"(abc"\"123\"")"_indent_0_drop_1, ""); }
+    auto formatted_raw_literal = R"(abc"\"123\"")"_indent_0_drop_1;
+    EXPECT_EQ(formatted_raw_literal, "");
+}
 TEST(indent_0_drop_1, two_lines_raw_literal) { EXPECT_EQ(R"(abc
 123)"_indent_0_drop_1, "123\n"); }
 TEST(indent_0_drop_1, two_lines_raw_literal_with_quotes) { EXPECT_EQ(R"(abc
 "123")"_indent_0_drop_1, R"("123")""\n"); }
-TEST(indent_0_drop_1, two_lines_raw_literal_with_quotes_and_escaped_quotes) { EXPECT_EQ(R"(abc
-"\"123\"")"_indent_0_drop_1, R"("\"123\"")""\n"); }
-
+TEST(indent_0_drop_1, two_lines_raw_literal_with_quotes_and_escaped_quotes) {
+    auto formatted_raw_literal = R"(abc
+"\"123\"")"_indent_0_drop_1;
+    auto expected_formatted_string_literal = R"("\"123\"")""\n";
+    EXPECT_EQ(formatted_raw_literal, expected_formatted_string_literal);
+}
 TEST(indent_4, empty_literal) { EXPECT_EQ(""_indent_4, ""); }
 TEST(indent_4, one_line_literal) { EXPECT_EQ("abc"_indent_4, "    abc\n"); }
 TEST(indent_4, two_lines_literal) { EXPECT_EQ("abc\n123"_indent_4, "    abc\n    123\n"); }
